17097f91bc1d0230d18e54789244e0a6f022ed56
skarni1805 <52778826+skarni1805@users.noreply.github.com>
2020-09-24 23:47:14 -0400

    Update resource.tf

--- APP1/UTest/resource.tf
+++ APP1/UTest/resource.tf
@@ -1,1 +1,67 @@
+variable "aws_region" {
+  type    = string
+  default = "us-west-1"
+}
+
+variable "db_table_name" {
+  type    = string
+  default = "terraform-learn"
+}
+
+variable "db_read_capacity" {
+  type    = number
+  default = 1
+}
+
+variable "db_write_capacity" {
+  type    = number
+  default = 1
+}
+
+variable "cidr_block" {
+    type = string
+    description = "CIDR block for VPC"
+    default = "10.0.0.0/16"
+}
+
+
+provider "aws" {
+  version = "2.33.0"
+
+  region = var.aws_region
+}
+
+provider "random" {
+  version = "2.2"
+}
+
+resource "random_pet" "table_name" {}
+
+resource "aws_dynamodb_table" "tfc_example_table" {
+  name = "${var.db_table_name}-${random_pet.table_name.id}"
+
+  read_capacity  = var.db_read_capacity
+  write_capacity = var.db_write_capacity
+  hash_key       = "UUID"
+  range_key      = "UserName"
+
+  attribute {
+    name = "UUID"
+    type = "S"
+  }
+
+  attribute {
+    name = "UserName"
+    type = "S"
+  }
+}
+
+resource "aws_vpc" "default" {
+  cidr_block = var.cidr_block
+}
+
+
+output "tfc_example_table_arn" {
+  value = aws_dynamodb_table.tfc_example_table.arn
+}
 
